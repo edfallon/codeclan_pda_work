88f3ab664a758ba856318333e25218db301a9a1b
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-22 16:47:03 +0100

    passing test for muliplying 3 and 5

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,12 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should equal 15 when multiplying 3 and 5', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+  })
+
 });
