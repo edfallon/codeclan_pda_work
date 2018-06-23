c7e35ca16a57c27f1e313e7dc782a99ee686c675
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-23 12:38:00 +0100

    passing test for the clear button not changing the operation

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -81,4 +81,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('20')
   })
 
+  it('should not affect the operation when clicking the clear button', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#clear')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6')
+  })
+
 });
