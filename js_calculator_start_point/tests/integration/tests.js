739d1085004c57ac1b292cae19db8c3fc5f91c7d
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-23 11:37:59 +0100

    passing test for divide 21 by 7

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -22,6 +22,17 @@ describe('calculator functionality', function() {
     element(by.css('#operator_multiply')).click();
     element(by.css('#number5')).click();
     element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('15')
+  })
+
+  it('should equal 3 when dividing 21 by 7', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
   })
 
 });
